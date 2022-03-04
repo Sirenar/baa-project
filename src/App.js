883f8454bbd97824bc6ac3bf0f6792783a139b55
Siren <siren.xrw@gmail.com>
2022-03-04 15:41:48 +0800

    新建项目

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Layout, Menu, Breadcrumb, Form, Steps } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+const { Step } = Steps;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Layout>
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+          <div className="logo" >
+            BONE AGE ACCESSMENT
+          </div>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+            <div className="form-title">
+              儿童骨龄评估
+            </div>
+            <div className="form-background">
+            <Steps current={1}>
+              <Step title="Finished" description="This is a description." />
+              <Step title="In Progress" subTitle="Left 00:00:08" description="This is a description." />
+              <Step title="Waiting" description="This is a description." />
+            </Steps>,
+              <Form>
+                <Button type="primary" >
+                  Choose File
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>,
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +58,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
