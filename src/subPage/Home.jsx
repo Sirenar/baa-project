5db41521101e6235a83c85df8d126a68a369d206
Siren <siren.xrw@gmail.com>
2022-03-06 16:54:17 +0800

    feat: table编辑功能

--- src/subPage/Home.jsx
+++ src/subPage/Home.jsx
@@ -1,80 +1,120 @@
-import React, { useState } from 'react';
 import './Home.css';
-import { Menu, Button, Form, Steps, Dropdown, Divider, Upload, Tooltip, Table, Row, Col } from 'antd';
+import React, { useState } from 'react';
+import { Button, Form, Divider, Upload, Tooltip, Table, Tag, Select, Space } from 'antd';
+import { PlusOutlined, ArrowRightOutlined } from '@ant-design/icons';
 
-const { Step } = Steps;
-
-const columns = [
-  {
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <a>{text}</a>,
-  },
-  {
-    dataIndex: 'age',
-    key: 'age',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age,
-  },
-  {
-    dataIndex: 'gender',
-    filters: [
-      { text: 'Male', value: 'male' },
-      { text: 'Female', value: 'female' },
-    ],
-    // specify the condition of filtering result
-    // here is that finding the name started with `value`
-    onFilter: (value, record) => record.gender.indexOf(value) === 0,
-
-  },
-  {
-    title: '影像图片',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: '操作',
-    key: 'action',
-    render: (text, record) => (
-      // <Space size="middle">
-      //   <a>Invite {record.name}</a>
-      //   <a>Delete</a>
-      // </Space>
-      <div>错做</div>
-    ),
-  },
-];
-
-const menu = (
-  <Menu>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
-        1st menu item
-      </a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
-        2nd menu item
-      </a>
-    </Menu.Item>
-    <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
-        3rd menu item
-      </a>
-    </Menu.Item>
-  </Menu>
-);
-
-// console.log('fileList: ', )
-// const onChange = info => {
-//   console.log(info.file, info.fileList);
-//   changeFileList(info.fileList);
-// }
-
-
+const { Option } = Select;
 
 function Home() {
+  const columns = [
+    {
+      dataIndex: 'a',
+      key: 'a',
+      width: '32px',
+    },
+    {
+      dataIndex: 'name',
+      key: 'name',
+      width: '440px',
+      ellipsis: true,
+      // render: text => <a>{text}</a>,
+    },
+    {
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      dataIndex: 'gender',
+    },
+    {
+      dataIndex: 'model',
+      key: 'model',
+      width: '200px',
+      render: model => {
+        return modelSelections(model)
+      }
+    },
+    {
+      dataIndex: 'status',
+      width: '120px',
+      align: 'center',
+      render: status => {
+        let color = '';
+        switch(status) {
+          case 'uploading':
+            color = 'geekblue';
+            break;
+          case 'done':
+            color = 'green';
+            break;
+          case 'error':
+            color = 'volcano';
+            break;
+          case 'removed':
+            color = 'volcano';
+        }
+        return (
+          <Tag color={color}>
+            {status.toUpperCase()}
+          </Tag>
+        )
+      }
+    },
+    {
+      dataIndex: 'size',
+      key: 'size',
+      width: '240px',
+      align: 'center',
+      className: 'size-font',
+      render: size => {
+        if (size>1024*1024) {
+          size = (size/1048576).toFixed(1) + 'MB';
+        } else if (size>1024) {
+          size = Math.round(size/1024) + 'KB';
+        }
+        return (
+          <div>{size}</div>
+        )
+      }
+    },
+    {
+      title: '操作',
+      key: 'action',
+      width: '80px',
+      render: _ => (
+        <span className='btn-close' onClick={() => handleDelete(_)}></span>
+      ),
+    },
+  ];
+
+  const modelSelections = currentModel => (
+    <>
+      <Space size="middle" className='model-selection-box'>
+        <span className='model-caption'>use</span>
+        <Select defaultValue={currentModel || '模型1'} style={{ width: 100 }} onChange={handleChange}>
+          <Option value="jack">模型1</Option>
+          <Option value="lucy">模型2</Option>
+          <Option value="disabled" disabled>
+            Disabled
+          </Option>
+          <Option value="Yiminghe">模型3</Option>
+        </Select>
+      </Space>
+    </>
+  );
+  
+  const handleDelete = ({uid}) => {
+    const dataSource = [...fileList];
+    changeFileList(fileList.filter(item => item.uid !== uid));
+  }
+
+  
+  const handleChange = value => {
+    console.log(`selected ${value}`);
+  }
+
   const [fileList, changeFileList] = useState([]);
+
   return (
     <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
       <div className="form-title">
@@ -82,7 +122,7 @@ function Home() {
         <h2>Get bone age accessment online & free</h2>
       </div>
       <div className='content-box'>
-      <Divider />
+      {/* <Divider /> */}
         {/* <Steps progressDot current={1} className="steps-box">
           <Step title="Finished" description="This is a description." />
           <Step title="In Progress" description="This is a description." />
@@ -99,20 +139,14 @@ function Home() {
           <div className='btn-holder'>
           <Upload
             action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            // fileList={fileList}
+            fileList={fileList}
             onChange={ info => {
-              console.log(info.file, info.fileList);
               changeFileList(info.fileList);
             }}
-            itemRender={(originNode, file, currFileList) => {
-              const errorNode = <Tooltip title="Upload Error">{originNode.props.children}</Tooltip>;
-                return (
-                  <div>
-                    {file.status === 'error' ? errorNode : originNode}
-                  </div> 
-              )
-            }}>
-            <Button className="file-btn" >
+            showUploadList={false}
+            multiple={true}
+            >
+            <Button className="file-btn" icon={<PlusOutlined style={{color: '#4f68b0'}}/>}>
               Choose File
             </Button>
           </Upload>
@@ -126,8 +160,10 @@ function Home() {
               <Button className="algo-btn">Model</Button>
             </Dropdown> */}
         <div className='submit-container'>
-          <span>评估</span>
-          {/* <svg></svg> */}
+          <span>评估结果</span>
+          <div style={{position: 'relative'}}>
+            <ArrowRightOutlined style={{color: 'white'}}/>
+          </div>
         </div>
       </Form>
     </div>
--- src/subPage/Records.jsx
+++ src/subPage/Records.jsx
@@ -1,106 +1,236 @@
-import { Table, Tag, Space } from 'antd';
+import './Records.css';
+import React, { useState } from 'react';
+import { Table, Input, InputNumber, Popconfirm, Form, Typography, Tag, Space } from 'antd';
+const originData = [];
 
-const columns = [
-  {
-    title: '姓名',
-    dataIndex: 'name',
-    key: 'name',
-    render: text => <a>{text}</a>,
-  },
-  {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age,
-  },
-  {
-    title: '性别',
-    dataIndex: 'gender',
-    filters: [
-      { text: 'Male', value: 'male' },
-      { text: 'Female', value: 'female' },
-    ],
-    // specify the condition of filtering result
-    // here is that finding the name started with `value`
-    onFilter: (value, record) => record.gender.indexOf(value) === 0,
+for (let i = 0; i < 100; i++) {
+  originData.push({
+    key: i.toString(),
+    name: `Edrward ${i}`,
+    age: 8+i,
+    gender: ['male', 'female'][i%2],
+    tags: [['cute', 'developer', 'loser', 'wealthy'][i%4]],
+    address: `London Park no. ${i}`,
+  });
+}
 
-  },
-  {
-    title: '影像图片',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: '引用数',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: tags => (
-      <>
-        {tags.map(tag => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: '操作',
-    key: 'action',
-    render: (text, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const inputNode = inputType === 'number' ? <InputNumber /> : <Input />;
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    gender: 'male',
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: '花花',
-    age: 12,
-    gender: 'female',
-    address: 'London No. 1 Lake Park',
-    tags: ['cute'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    gender: 'male',
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-];
+const Records = () => {
+  const [form] = Form.useForm();
+  const [data, setData] = useState(originData);
+  const [editingKey, setEditingKey] = useState('');
 
+  const isEditing = (record) => record.key === editingKey;
 
-function Records() {
-    return (
-     <div>
-      <Table 
-        columns={columns} 
-        dataSource={data} 
-        pagination={{ position: 'bottomLeft' }}
+  const handleEdit = (record) => {
+    form.setFieldsValue({
+      name: '',
+      age: '',
+      address: '',
+      ...record,
+    });
+    setEditingKey(record.key);
+  };
+
+  const cancel = () => {
+    setEditingKey('');
+  };
+
+  const save = async (key) => {
+    try {
+      const row = await form.validateFields();
+      const newData = [...data];
+      const index = newData.findIndex((item) => key === item.key);
+
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        setData(newData);
+        setEditingKey('');
+      } else {
+        newData.push(row);
+        setData(newData);
+        setEditingKey('');
+      }
+    } catch (errInfo) {
+      console.log('Validate Failed:', errInfo);
+    }
+  };
+
+  const handleDelete = key => {
+    console.log(key)
+    // form.setFieldsValue({
+    //   name: '',
+    //   age: '',
+    //   address: '',
+    //   ...record,
+    // });
+    // setEditingKey(record.key);
+  };
+
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+      editable: true,
+      width: '150px',
+      align: 'center',
+      // render: text => <a>{text}</a>,
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+      editable: true,
+      width: '100px',
+      align: 'center',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: '性别',
+      dataIndex: 'gender',
+      key: 'gender',
+      editable: true,
+      width: '150px',
+      align: 'center',
+      filters: [
+        { text: 'Male', value: 'male' },
+        { text: 'Female', value: 'female' },
+      ],
+      render: gender => <div>{gender.toLowerCase()==='female'?'女':'男'}</div>
+    },
+    {
+      title: '影像图片',
+      dataIndex: 'address',
+      key: 'address',
+      editable: true,
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: '引用数',
+      key: 'tags',
+      dataIndex: 'tags',
+      width: '200px',
+      align: 'center',
+      render: tags => (
+        <>
+          {tags.map(tag => {
+            let color = tag.length > 7 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      width: '200px',
+      align: 'center',
+      render: (_, record) => {
+        const editable = isEditing(record);
+        return editable ? (
+          <span>
+            <Typography.Link
+              onClick={() => save(record.key)}
+              style={{
+                marginRight: 8,
+              }}
+            >
+              Save
+            </Typography.Link>
+            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+              <a>Cancel</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <Space size="middle">
+            <Typography.Link disabled={editingKey !== ''} onClick={() => handleEdit(record)}>
+              修改
+            </Typography.Link>
+            <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
+              <a>Delete</a>
+            </Popconfirm>
+          </Space>
+        );
+      },
+    },
+  ];
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.dataIndex === 'age' ? 'number' : 'text',
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: isEditing(record),
+      }),
+    };
+  });
+  return (
+    <Form form={form} component={false}>
+      <Table
+        components={{
+          body: {
+            cell: EditableCell,
+          },
+        }}
+        dataSource={data}
+        columns={mergedColumns}
+        rowClassName="editable-row"
+        pagination={{
+          onChange: cancel,
+        }}
       />
-     </div>
-    );
-  }
+    </Form>
+  );
+};
 
 export default Records;
